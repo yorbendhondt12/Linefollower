c8ce3f0657aef1111e149be7a33f6084f144e600
yorbendhondt12 <146442844+yorbendhondt12@users.noreply.github.com>
2023-10-04 14:48:13 +0200

    Add files via upload

---
+++ code/proof of concepts/interrupt/start-stop_interrupt.ino
@@ -0,0 +1,24 @@
+const int buttonPin = 2;
+const int ledPin = 4;
+volatile int buttonState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT); // Interne pull-up weerstand inschakelen
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, FALLING);
+}
+
+void loop() {
+  // De hoofdcode hier kan andere taken uitvoeren terwijl het wacht op de interrupt
+}
+
+void buttonInterrupt() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("High");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("Low");
+  }
+}
